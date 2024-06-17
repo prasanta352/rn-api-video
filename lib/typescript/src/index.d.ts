4806c77337b9b57d932ea50398bf1f45fef090f4
Prasanta Barman <prasantabarman06@gmail.com>
2024-06-17 19:19:20 +0530

    feat: added libs

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,12 @@
+import { type ViewStyle } from 'react-native';
+type RnApiVideoProps = {
+    color: string;
+    videoId: string;
+    style: ViewStyle;
+    autoplay?: boolean;
+    isPlaying?: boolean;
+    isMuted?: boolean;
+};
+export declare const RnApiVideoView: import("react-native").HostComponent<RnApiVideoProps> | (() => never);
+export {};
+//# sourceMappingURL=index.d.ts.map
